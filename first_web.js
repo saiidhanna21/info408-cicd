ce55fc8df63b194461274ced18abc032454e9a65
Saiid Hanna <hanna.saiid2003@gmail.com>
2024-05-30 08:39:26 +0300

    First Commit

---
+++ first_web.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({
+        status: 200,
+        message: "Testing My First Website using CiCd Pipeline"
+    })
+})
+
+app.listen(8000, () => {
+    console.log("Server Running on port 8000")
+})
